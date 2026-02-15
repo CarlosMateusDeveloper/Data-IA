0d31264e18321309418ed753fb15440f81410847
Carlos Mateus <mateusdev33@gmail.com>
2026-02-15 20:10:27 -0300

    refactor add_sheet.py add for loop and save sheet

--- add_sheet.py
+++ add_sheet.py
@@ -5,9 +5,9 @@ from openpyxl import Workbook, load_workbook
 def add_sheet( sheet_file):
     df = extract()
     book = load_workbook(sheet_file)
-    ws = book[sheet_file]
-    primeira_linha = ws.max_row + 1
+    ws = book['planilha1']
     
+    for i in df.values:
+        ws.append(i)
     
-    
-    return 
+    book.save(sheet_file)
